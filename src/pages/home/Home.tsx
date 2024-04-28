61d67d286697d1c867ff19c897f7cb9244ade20a
PepeAliasJose <peperc781@gmail.com>
2024-04-28 20:34:10 +0200

    Description
    description added

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,14 +7,33 @@ const Home = () => {
   const { t } = useTranslation("global");
 
   return (
-    <Box m={"10px"}>
-      <Text fontSize={{ base: "3xl", md: "5xl" }}>{t("home.name")}</Text>
-      <List>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit
-        </ListItem>
-      </List>
+    <Box m={"10px"} display={"flex"} flexDirection={"column"}>
+      <Box display={"block"}>
+        <Text fontSize={{ base: "3xl", md: "5xl" }}>{t("home.name")}</Text>
+        <List fontSize={"sm"}>
+          <ListItem>
+            <ListIcon as={CheckCircleIcon} color="blue.500" />
+            {t("home.description.dev")}
+          </ListItem>
+          <ListItem>
+            <ListIcon as={CheckCircleIcon} color="blue.500" />
+            {t("home.description.rob")}
+          </ListItem>
+          <ListItem>
+            <ListIcon as={CheckCircleIcon} color="blue.500" />
+            {t("home.description.fot")}
+          </ListItem>
+        </List>
+      </Box>
+      <Box alignSelf={"end"}>1</Box>
+      <Box alignSelf={"start"}>2</Box>
+      <Box
+        w={{ base: "100%", sm: "75%", md: "45%" }}
+        ml={"auto"}
+        alignSelf={"end"}
+      >
+        <Text fontSize={"md"}>{t("home.longDescription")}</Text>
+      </Box>
     </Box>
   );
 };
