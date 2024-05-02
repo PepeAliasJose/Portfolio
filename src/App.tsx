04ff6d07e257a294ab2f191366677672fccb878d
PepeAliasJose <peperc781@gmail.com>
2024-05-02 09:51:32 +0200

    del firebase
    del firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import Navbar from "./components/molecules/Navbar";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -15,21 +10,6 @@ import Robotic from "./pages/robotics/Robotic";
 import Contact from "./pages/contact/Contact";
 
 const App = () => {
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBUsOy7EczMc9t7QWVuMNxSZNCZnHCMFl8",
-    authDomain: "jose-rodriguez-caceres.firebaseapp.com",
-    projectId: "jose-rodriguez-caceres",
-    storageBucket: "jose-rodriguez-caceres.appspot.com",
-    messagingSenderId: "610281082622",
-    appId: "1:610281082622:web:b8d19b1ef3fb8005cd5faf",
-    measurementId: "G-KC0LHG97CJ",
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
   return (
     <BrowserRouter>
       <Navbar />
