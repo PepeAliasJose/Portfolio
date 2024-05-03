f7ff8dde8300eb4654a4dee52d1ad94aa3d48581
PepeAliasJose <peperc781@gmail.com>
2024-05-03 08:39:31 +0200

    Add analitics

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 import Navbar from "./components/molecules/Navbar";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -10,6 +15,18 @@ import Robotic from "./pages/robotics/Robotic";
 import Contact from "./pages/contact/Contact";
 
 const App = () => {
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_SENDER_ID,
+    appId: process.env.REACT_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  getAnalytics(app);
   return (
     <BrowserRouter>
       <Navbar />
